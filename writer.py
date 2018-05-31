0d3fd4ffc6d00ea42777b8144512669bf9d42652
樊建 <971797140@qq.com>
2018-05-31 11:46:24 +0800

    跳过不执行的邮件

--- writer.py
+++ writer.py
@@ -13,9 +13,21 @@ print("start save new case.")
 index = 0
 
 for i in range(len(mails)):
+    # 数字替换为d打头单词的，认为是无效的mail
+    if mails[i].strip()[:1].lower() == "d":
+        continue
+
     lines = [l for l in mails[i].split("\n") if l.strip() != ""]
     if len(lines) == 0:
         continue
+
+    # 如果当前邮件已经分配过了，就跳过不执行
+    hasPostedCreator = False
+    for l in lines:
+        if l.find(case.__PostedCreator__) > 0 and l.strip() != "":
+            hasPostedCreator = True
+    if hasPostedCreator: continue
+
     index += 1
     print("mail: {}".format(index))
     case.doNewCase(lines, access)
