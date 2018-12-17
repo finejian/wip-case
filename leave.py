e08e010d310e4ed76c71a0dea39255e1ee8b3bc4
Jamesfan <jamesfan1010@gmail.com>
2018-12-17 23:14:19 +0800

    改回默认mdb.path

--- leave.py
+++ leave.py
@@ -86,7 +86,7 @@ if __name__ == '__main__':
     l = Leave()
     print("members: ", l.members)
     member = "catherine"
-    print(member, "leave: ", l.memberdaysLeave(member, ['2018-11-15', '2018-11-16']))
+    print(member, "leave: ", l.memberDaysLeave(member, ['2018-11-15', '2018-11-16']))
     print("all member total leave: ", l.daysTotalLeave(['2018-11-15', '2018-11-16']))
     print("all member total hour: ", l.daysTotalHour(['2018-11-15', '2018-11-16']))
  
